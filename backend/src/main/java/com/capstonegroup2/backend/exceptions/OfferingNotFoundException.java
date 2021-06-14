f140a3f2056c2d9b3345052ae316b3b0da2fc74e
David Amateau <damateau@gmail.com>
2021-06-14 14:25:41 -0400

    switched to CommandLineRunner to seed cd offerings from constants in MeritBank

---
+++ backend/src/main/java/com/capstonegroup2/backend/exceptions/OfferingNotFoundException.java
@@ -0,0 +1,12 @@
+package com.capstonegroup2.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class OfferingNotFoundException extends Exception{
+
+    public OfferingNotFoundException() {
+        super("The CDOffering could not be located.");
+    }
+}
