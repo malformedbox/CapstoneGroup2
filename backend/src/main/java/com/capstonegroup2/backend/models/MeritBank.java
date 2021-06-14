265b0902a28535b50360a4653a2f883aa5d69f6b
David Amateau <damateau@gmail.com>
2021-06-14 14:38:37 -0400

    switched to CommandLineRunner to seed cd offerings from constants in MeritBank

--- backend/src/main/java/com/capstonegroup2/backend/models/MeritBank.java
+++ backend/src/main/java/com/capstonegroup2/backend/models/MeritBank.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class MeritBank {
 
-    // I've created this class so we can easily see and manipulate standard business
-    // logic variables that will be used by Merit Bank
+
     public static final String DBA_CHECKING_INTEREST_RATE = "0.0005";
     public static final String PERSONAL_CHECKING_INTEREST_RATE = ".0003";
     public static final String IRA_REGULAR_INTEREST_RATE = "0.65";
     public static final String IRA_ROLLOVER_INTEREST_RATE = "0.08";
     public static final String IRA_ROTH_INTEREST_RATE = "0.08";
     public static final String SAVINGS_ACCOUNT_INTEREST_RATE = "0.01";
-
     public static List<CDOffering> cdOfferings = currentOfferings();
+    public static final double TRANSACTION_LIMIT = 20000.00;
+
 
     private static List<CDOffering> currentOfferings() {
+
         List<CDOffering> offerings = new ArrayList<>();
+
         CDOffering offer1 = new CDOffering(1, "0.018");
         CDOffering offer2 = new CDOffering(3, "0.02");
         CDOffering offer3 = new CDOffering(5, "0.023");
@@ -30,5 +32,5 @@ public class MeritBank {
         return offerings;
     }
 
-    public static final double TRANSACTION_LIMIT = 20000.00;
+
 }
